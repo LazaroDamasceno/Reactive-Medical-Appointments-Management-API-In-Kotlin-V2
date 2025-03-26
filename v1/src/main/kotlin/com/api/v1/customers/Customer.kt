829f6f48e54572510f4bca11fdf5afb6e2f37a72
lazarodamasceno <lazarolawrence2@gmail.com>
2025-03-25 22:41:43 -0300

    Created the entity Customer

---
+++ v1/src/main/kotlin/com/api/v1/customers/Customer.kt
@@ -0,0 +1,31 @@
+package com.api.v1.customers
+
+import com.api.v1.common.Address
+import com.api.v1.people.domain.Person
+import org.bson.codecs.pojo.annotations.BsonId
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Document
+class Customer {
+
+    @BsonId
+    var id: String = UUID.randomUUID().toString()
+    lateinit var person: Person
+    lateinit var address: Address
+    var createdAt: LocalDateTime = LocalDateTime.now()
+
+    constructor()
+
+    private constructor(person: Person, address: Address) {
+        this.person = person
+        this.address = address
+    }
+
+    companion object {
+        fun of(person: Person, address: Address): Customer {
+            return Customer(person, address)
+        }
+    }
+}
