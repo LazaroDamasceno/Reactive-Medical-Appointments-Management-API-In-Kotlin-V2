bd8a189de2bcf59416b0a7e10d25a8bf4365199e
lazarodamasceno <lazarolawrence2@gmail.com>
2025-03-26 04:18:38 -0300

    Created the service DoctorRetrievalService

---
+++ v1/src/main/kotlin/com/api/v1/doctors/services/DoctorRetrievalService.kt
@@ -0,0 +1,10 @@
+package com.api.v1.doctors.services
+
+import com.api.v1.doctors.dtos.DoctorResponseDto
+import kotlinx.coroutines.flow.Flow
+import org.springframework.http.ResponseEntity
+
+interface DoctorRetrievalService {
+    suspend fun findByMedicalLicenseNumber(licenseNumber: String, state: String): ResponseEntity<DoctorResponseDto>
+    suspend fun findAll(): ResponseEntity<Flow<DoctorResponseDto>>
+}
