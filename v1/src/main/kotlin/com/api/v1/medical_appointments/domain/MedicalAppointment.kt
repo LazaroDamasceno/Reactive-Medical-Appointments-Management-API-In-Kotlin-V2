0c9b98d7e5da298f94a6f342c8a8c4b986dc1f1f
lazarodamasceno <lazarolawrence2@gmail.com>
2025-03-27 21:34:22 -0300

    Created the entity MedicalAppointment

---
+++ v1/src/main/kotlin/com/api/v1/medical_appointments/domain/MedicalAppointment.kt
@@ -0,0 +1,23 @@
+package com.api.v1.medical_appointments.domain
+
+import com.api.v1.customers.domain.exposed.Customer
+import com.api.v1.doctors.domain.exposed.Doctor
+import org.bson.codecs.pojo.annotations.BsonId
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+import java.util.*
+
+@Document
+class MedicalAppointment(
+    var customer: Customer,
+    var doctor: Doctor,
+    var bookedAt: LocalDateTime
+) {
+
+    @BsonId
+    var id: String = UUID.randomUUID().toString()
+    var createdAt: LocalDateTime = LocalDateTime.now()
+    var canceledAt: LocalDateTime? = null
+    var completedAt: LocalDateTime? = null
+
+}
