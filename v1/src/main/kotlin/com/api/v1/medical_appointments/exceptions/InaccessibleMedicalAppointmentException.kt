afeaa48678cd56683c7ec300ef66ea85491b429a
lazarodamasceno <lazarolawrence2@gmail.com>
2025-04-03 02:34:39 -0300

    Added one extra validation for MedicalAppointmentRegistrationService

--- v1/src/main/kotlin/com/api/v1/medical_appointments/exceptions/InaccessibleMedicalAppointmentException.kt
+++ v1/src/main/kotlin/com/api/v1/medical_appointments/exceptions/InaccessibleMedicalAppointmentException.kt
@@ -1,6 +1,13 @@
 package com.api.v1.medical_appointments.exceptions
 
-class InaccessibleMedicalAppointmentException(id: String)
-    : RuntimeException("""
-            Customer whose id is $id not associated with sought medical appointment.
-        """)
+class InaccessibleMedicalAppointmentException : RuntimeException {
+
+    constructor(customerId: String) : super("""
+        Customer whose id is $customerId not associated with sought medical appointment.
+    """)
+
+    constructor() : super("""
+        Doctor is already the medical slot's doctor of the sought medical appointment.
+    """)
+
+}
--- v1/src/main/kotlin/com/api/v1/medical_appointments/services/MedicalAppointmentRegistrationServiceImpl.kt
+++ v1/src/main/kotlin/com/api/v1/medical_appointments/services/MedicalAppointmentRegistrationServiceImpl.kt
@@ -5,10 +5,12 @@ import com.api.v1.common.PastBookingDateTimeException
 import com.api.v1.common.UnavailableBookingDateTimeException
 import com.api.v1.customers.domain.exposed.Customer
 import com.api.v1.customers.utils.CustomerFinder
+import com.api.v1.doctors.domain.exposed.Doctor
 import com.api.v1.doctors.utils.DoctorFinder
 import com.api.v1.medical_appointments.domain.MedicalAppointmentRepository
 import com.api.v1.medical_appointments.domain.exposed.MedicalAppointment
 import com.api.v1.medical_appointments.dtos.MedicalAppointmentResponseDto
+import com.api.v1.medical_appointments.exceptions.InaccessibleMedicalAppointmentException
 import com.api.v1.medical_appointments.utils.MedicalAppointmentFinder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -33,7 +35,7 @@ class MedicalAppointmentRegistrationServiceImpl(
         return withContext(Dispatchers.IO) {
             val foundDoctor = doctorFinder.findByMedicalLicenseNumber(licenseNumber, state)
             val foundCustomer = customerFinder.findById(customerId)
-            validate(foundCustomer, bookedAt)
+            validate(foundCustomer, foundDoctor, bookedAt)
             val newMedicalAppointment = MedicalAppointment.of(foundCustomer, foundDoctor, bookedAt)
             val savedMedicalAppointment = medicalAppointmentRepository.save(newMedicalAppointment)
             val dto = savedMedicalAppointment.toDto()
@@ -41,13 +43,18 @@ class MedicalAppointmentRegistrationServiceImpl(
         }
     }
 
-    private suspend fun validate(customer: Customer, bookedAt: LocalDateTime) {
+    private suspend fun validate(customer: Customer, doctor: Doctor, bookedAt: LocalDateTime) {
         if (medicalAppointmentFinder.find(customer, bookedAt) != null) {
             throw UnavailableBookingDateTimeException(bookedAt)
         }
 
         if (PastBookingDateTimeChecker.isBeforeToday(bookedAt.toLocalDate())) {
             throw PastBookingDateTimeException()
+        }
+
+        if (doctor.person.id == customer.id) {
+            val message = "Doctor is the med"
+            throw InaccessibleMedicalAppointmentException()
         }
     }
 }
