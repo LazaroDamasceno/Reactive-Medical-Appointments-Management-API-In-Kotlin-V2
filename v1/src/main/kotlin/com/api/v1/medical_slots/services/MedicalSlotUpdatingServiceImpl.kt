8730b6208a4a74a4add1f7e887f25c4dea1ad078
lazarodamasceno <lazarolawrence2@gmail.com>
2025-04-03 03:14:11 -0300

    Removed from MedicalSlotUpdatingService the method 'set(medicalSlot: MedicalSlot, medicalAppointment: MedicalAppointment)'

--- v1/src/main/kotlin/com/api/v1/medical_slots/services/MedicalSlotUpdatingServiceImpl.kt
+++ v1/src/main/kotlin/com/api/v1/medical_slots/services/MedicalSlotUpdatingServiceImpl.kt
@@ -1,6 +1,5 @@
 package com.api.v1.medical_slots.services
 
-import com.api.v1.medical_appointments.domain.exposed.MedicalAppointment
 import com.api.v1.medical_slots.domain.MedicalSlot
 import com.api.v1.medical_slots.domain.MedicalSlotRepository
 import com.api.v1.medical_slots.services.exposed.MedicalSlotUpdatingService
@@ -15,13 +14,6 @@ class MedicalSlotUpdatingServiceImpl(
 
     override suspend fun set(medicalSlot: MedicalSlot) {
         return withContext(Dispatchers.IO) {
-            medicalSlotRepository.save(medicalSlot)
-        }
-    }
-
-    override suspend fun set(medicalSlot: MedicalSlot, medicalAppointment: MedicalAppointment) {
-        return withContext(Dispatchers.IO) {
-            medicalSlot.medicalAppointment = medicalAppointment
             medicalSlotRepository.save(medicalSlot)
         }
     }
--- v1/src/main/kotlin/com/api/v1/medical_slots/services/exposed/MedicalSlotUpdatingService.kt
+++ v1/src/main/kotlin/com/api/v1/medical_slots/services/exposed/MedicalSlotUpdatingService.kt
@@ -1,9 +1,7 @@
 package com.api.v1.medical_slots.services.exposed
 
-import com.api.v1.medical_appointments.domain.exposed.MedicalAppointment
 import com.api.v1.medical_slots.domain.MedicalSlot
 
 interface MedicalSlotUpdatingService {
     suspend fun set(medicalSlot: MedicalSlot)
-    suspend fun set(medicalSlot: MedicalSlot, medicalAppointment: MedicalAppointment)
 }
