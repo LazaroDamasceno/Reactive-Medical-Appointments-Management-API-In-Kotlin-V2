63941bebf4955abb4b918b031024f49b6fd2c881
lazarodamasceno <lazarolawrence2@gmail.com>
2025-04-04 14:05:54 -0300

    Created the utility CardFinder

--- v1/src/main/kotlin/com/api/v1/cards/services/CardRegistrationServiceImpl.kt
+++ v1/src/main/kotlin/com/api/v1/cards/services/CardRegistrationServiceImpl.kt
@@ -6,6 +6,8 @@ import com.api.v1.cards.dtos.CardResponseDto
 import com.api.v1.cards.enums.CardType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
@@ -14,23 +16,24 @@ class CardRegistrationServiceImpl(
     private val cardRepository: CardRepository
 ): CardRegistrationService {
 
-    override suspend fun creditCard(cvc: String, dueDate: LocalDate): CardResponseDto {
+    override suspend fun creditCard(cvc: String, dueDate: LocalDate): ResponseEntity<CardResponseDto> {
         return withContext(Dispatchers.IO) {
             register(CardType.CREDIT_CARD, cvc, dueDate)
         }
     }
 
-    override suspend fun debitCard(cvc: String, dueDate: LocalDate): CardResponseDto {
+    override suspend fun debitCard(cvc: String, dueDate: LocalDate): ResponseEntity<CardResponseDto> {
         return withContext(Dispatchers.IO) {
             register(CardType.DEBIT_CARD, cvc, dueDate)
         }
     }
 
-    private suspend fun register(type: CardType, cvc: String, dueDate: LocalDate): CardResponseDto {
+    private suspend fun register(type: CardType, cvc: String, dueDate: LocalDate): ResponseEntity<CardResponseDto> {
         return withContext(Dispatchers.IO) {
             val card = Card.of(type, cvc, dueDate)
             val savedCard = cardRepository.save(card)
-            savedCard.toDto()
+            val dto = savedCard.toDto()
+            ResponseEntity.status(HttpStatus.CREATED).body(dto)
         }
     }
 }
