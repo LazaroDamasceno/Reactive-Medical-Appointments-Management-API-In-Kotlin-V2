cf325b67e7fcb163438150cf0006677eb6c7cac3
lazarodamasceno <lazarolawrence2@gmail.com>
2025-04-14 22:36:08 -0300

    Created the testing classes DebitCardRegistrationIntegrationTest and CreditCardRegistrationIntegrationTest

---
+++ v2/src/test/kotlin/com/api/v2/cards/CreditCardRegistrationIntegrationTest.kt
@@ -0,0 +1,27 @@
+package com.api.v2.cards
+
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class CreditCardRegistrationIntegrationTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun testSuccessful() {
+        webTestClient
+            .post()
+            .uri("api/v1/cards/credit-card")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful
+    }
+
+}
